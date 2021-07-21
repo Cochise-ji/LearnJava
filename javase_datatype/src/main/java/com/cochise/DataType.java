9335c9b084d7d445bc22a17c90b3a360072e1996
Cochise-ji <763383340@qq.com>
2021-07-21 14:00:23 +0800

    2021/07/21 javase（八大基本数据类型）

---
+++ javase_datatype/src/main/java/com/cochise/DataType.java
@@ -0,0 +1,36 @@
+package com.cochise;
+
+
+/**
+ * 自动类型转换（隐式）：从小到大
+ * 强制类型转换（显示）：从大到小
+ * byte->short->int->long->float->double
+ *
+ */
+public class DataType {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("基本类型：byte 二进制位数：" + Byte.SIZE);
+
+        // short
+        System.out.println("基本类型：short 二进制位数：" + Short.SIZE);
+
+        // int
+        System.out.println("基本类型：int 二进制位数：" + Integer.SIZE);
+
+        // long
+        System.out.println("基本类型：long 二进制位数：" + Long.SIZE);
+
+        // float
+        System.out.println("基本类型：float 二进制位数：" + Float.SIZE);
+
+        // double
+        System.out.println("基本类型：double 二进制位数：" + Double.SIZE);
+
+        // char
+        System.out.println("基本类型：char 二进制位数：" + Character.SIZE);
+
+        // bool
+        System.out.println("基本类型：bool 二进制位数：1");
+    }
+}
