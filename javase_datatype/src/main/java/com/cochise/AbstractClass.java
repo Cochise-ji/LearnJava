26a413619e497eb090678ecd6373e2c7940dcc24
Cochise-ji <763383340@qq.com>
2021-07-21 16:04:35 +0800

    2021/07/21 1111

---
+++ javase_datatype/src/main/java/com/cochise/AbstractClass.java
@@ -0,0 +1,16 @@
+package com.cochise;
+
+/**
+ * 1，抽象类不能创建对象。只能创建其非抽象子类的对象。
+ * 2，抽象类中可以有构造方法，仅供子类创建对象时自动调用。
+ * 3，抽象类中不一定包含抽象方法，但是有抽象方法的类一定是抽象类。
+ * 4，抽象类的子类，必须重写所有抽象方法，否则该子类还得是抽象类。
+ */
+public abstract class AbstractClass {
+    // 无参构造方法
+    public AbstractClass() {
+    }
+
+    public abstract void test01();
+    public abstract void test02();
+}
