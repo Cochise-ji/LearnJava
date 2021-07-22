f4d1d885d7b48ba9be3471aa953556d8b48f18c3
Cochise-ji <763383340@qq.com>
2021-07-22 14:21:42 +0800

    2021/07/22 1111

---
+++ javase/src/main/java/org/cochise/Point05_polymorphism/Parent.java
@@ -0,0 +1,9 @@
+package org.cochise.Point05_polymorphism;
+
+public class Parent {
+    int age = 30;
+
+    public void showAge(){
+        System.out.println(this.age);
+    }
+}
---
+++ javase/src/main/java/org/cochise/Point05_polymorphism/Son.java
@@ -0,0 +1,38 @@
+package org.cochise.Point05_polymorphism;
+
+
+/**
+ * 对象向上转型（多态）
+ * 格式： 父类名称 对象名 = new 子类名称（）
+ * 格式： 接口名称 对象名 = new 实现类名称（）
+ *
+ * 对象向下转型 (还原)
+ * 格式： 子类名称 对象名 = （子类名称）父类对象
+ */
+public class Son extends Parent{
+    int age = 10;
+
+    @Override
+    public void showAge(){
+        System.out.println(this.age);
+    }
+
+    public void paly(){
+
+    }
+    public static void main(String[] args) {
+        Parent parent = new Son();
+
+        // 成员变量看左
+        System.out.println(parent.age);
+
+        // 成员方法看右
+        parent.showAge();
+        // 只能调用子类从父类继承的方法，子类特定的方法无法调用
+        // parent.play();
+
+        // 向下转型的对象必须是向上转型过来的，且只能转型成向上转型前的类型
+        Son son = (Son)parent;
+        son.paly();
+    }
+}
